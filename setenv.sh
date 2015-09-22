f961320a784795f0e93a6c09d8585363a9e541a9
gauthiier <d@gauthiier.info>
2015-09-22 13:50:11 +0200

    set env script added

---
+++ setenv.sh
@@ -0,0 +1,12 @@
+#var
+ANT=<edit>
+ANDROID=<edit>
+XWALK=<edit>
+#ANT
+export PATH=$ANT/bin:$PATH
+#ANDROID
+export PATH=$ANDROID:$PATH
+export PATH=$ANDROID/tools:$PATH
+export PATH=$ANDROID/platform-tools:$PATH
+#CROSSWALK
+export PATH=$XWALK:$PATH
